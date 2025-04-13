33b68db0d0ae8d69da30f5a1477b3d4d0060c815
dalmosantos <dalmo.santos@gmail.com>
2025-04-13 01:25:27 -0300

    fix test

--- app/src/androidTest/java/com/example/helloworld/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/helloworld/MainActivityTest.kt
@@ -6,6 +6,7 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.CoreMatchers.allOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -40,10 +41,12 @@ class MainActivityTest {
     @Test
     fun buttonShouldBeVisible_andClickable() {
         onView(withId(R.id.btnGreet))
-            .check(matches(allOf(
-                isDisplayed(),
-                isClickable()
-            )))
+            .check(matches(
+                allOf(
+                    isDisplayed(),
+                    isClickable()
+                )
+            ))
     }
 
     @Test
